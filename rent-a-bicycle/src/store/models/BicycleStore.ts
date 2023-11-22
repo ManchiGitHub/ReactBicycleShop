a09747aabe20b4582a7bc4526c57a82d1d99a809
ManchiGitHub <btllt13@gmail.com>
2023-11-22 21:57:07 +0200

    remove code smell

--- rent-a-bicycle/src/store/models/BicycleStore.ts
+++ rent-a-bicycle/src/store/models/BicycleStore.ts
@@ -29,71 +29,124 @@ export const BicycleStore = types.model({
             return results;
         }
     }))
-    .actions(self => ({
-        fetchBicycles: flow(function* () {
-            yield bicycleService.fetchBicycles().then((bicycles: any) => {
-                try {
-                    if (bicycles) {
-                        applySnapshot(self.bicycles, bicycles);
-                    }
-                } catch (eror) {
+    .actions(self => {
 
+        function fetchAndUpdateBicycles() {
+            return bicycleService.fetchBicycles().then((bicycles: any) => {
+                if (bicycles) {
+                    applySnapshot(self.bicycles, bicycles);
                 }
             })
-        }),
-        fetchUsers: flow(function* () {
-            yield userService.fetchUsers().then((users: any) => {
+                .catch(error => console.error("Error fetching bicycles:", error));
+        }
+
+        return {
+            fetchBicycles: flow(function* () {
+                yield fetchAndUpdateBicycles();
+            }),
+            turnLightsOff: flow(function* (bicycles: IBicycle[]) {
                 try {
-                    if (users) {
-                        applySnapshot(self.users, users)
-                    }
+                    yield bicycleService.updateBicycleLights(bicycles, false);
+                    yield fetchAndUpdateBicycles();
                 } catch (error) {
-
+                    console.error("Failed to turn off lights:", error);
                 }
-            })
-        }),
-        turnLightsOff: flow(function* (bicycles: IBicycle[]) {
-            try {
-                yield bicycleService.updateBicycleLights(bicycles, false);
-                yield bicycleService.fetchBicycles().then((bicycles: any) => {
+            }),
+            turnLightsOn: flow(function* (bicycles: IBicycle[]) {
+                try {
+                    yield bicycleService.updateBicycleLights(bicycles, true);
+                    yield fetchAndUpdateBicycles();
+                } catch (error) {
+                    console.error("Failed to turn on lights:", error);
+                }
+            }),
+            fetchUsers: flow(function* () {
+                yield userService.fetchUsers().then((users: any) => {
                     try {
-                        if (bicycles) {
-                            applySnapshot(self.bicycles, bicycles);
-                        }
-                    } catch (eror) {
-
-                    }
-                })
-            } catch (error) {
-                console.error("Failed to turn on all lights:", error);
-            }
-        }),
-        turnLightsOn: flow(function* (bicycles: IBicycle[]) {
-            try {
-                yield bicycleService.updateBicycleLights(bicycles, true);
-                yield bicycleService.fetchBicycles().then((bicycles: any) => {
-                    try {
-                        if (bicycles) {
-                            applySnapshot(self.bicycles, bicycles);
+                        if (users) {
+                            applySnapshot(self.users, users)
                         }
                     } catch (error) {
-                        console.log("Error while turning lights on", error);
+
                     }
                 })
-            } catch (error) {
-                console.error("Failed to turn on all lights:", error);
+            }),
+            setBicycleSearchQuery(query: string) {
+                self.searchBicycleQuery = query;
+            },
+            setUserSearchQuery(query: string) {
+                self.searchUserQuery = query;
+            },
+            setNavtitle(title: string) {
+                self.navigationTitle = title;
             }
-        }),
-        setBicycleSearchQuery(query: string) {
-            self.searchBicycleQuery = query;
-        },
-        setUserSearchQuery(query: string) {
-            self.searchUserQuery = query;
-        },
-        setNavtitle(title: string) {
-            self.navigationTitle = title;
         }
-    }))
+
+
+        // fetchBicycles: flow(function* () {
+        //     yield bicycleService.fetchBicycles().then((bicycles: any) => {
+        //         try {
+        //             if (bicycles) {
+        //                 applySnapshot(self.bicycles, bicycles);
+        //             }
+        //         } catch (eror) {
+
+        //         }
+        //     })
+        // }),
+        // fetchUsers: flow(function* () {
+        //     yield userService.fetchUsers().then((users: any) => {
+        //         try {
+        //             if (users) {
+        //                 applySnapshot(self.users, users)
+        //             }
+        //         } catch (error) {
+
+        //         }
+        //     })
+        // }),
+        // turnLightsOff: flow(function* (bicycles: IBicycle[]) {
+        //     try {
+        //         yield bicycleService.updateBicycleLights(bicycles, false);
+        //         yield bicycleService.fetchBicycles().then((bicycles: any) => {
+        //             try {
+        //                 if (bicycles) {
+        //                     applySnapshot(self.bicycles, bicycles);
+        //                 }
+        //             } catch (eror) {
+
+        //             }
+        //         })
+        //     } catch (error) {
+        //         console.error("Failed to turn on all lights:", error);
+        //     }
+        // }),
+        // turnLightsOn: flow(function* (bicycles: IBicycle[]) {
+        //     try {
+        //         yield bicycleService.updateBicycleLights(bicycles, true);
+        //         yield bicycleService.fetchBicycles().then((bicycles: any) => {
+        //             try {
+        //                 if (bicycles) {
+        //                     applySnapshot(self.bicycles, bicycles);
+        //                 }
+        //             } catch (error) {
+        //                 console.log("Error while turning lights on", error);
+        //             }
+        //         })
+        //     } catch (error) {
+        //         console.error("Failed to turn on all lights:", error);
+        //     }
+        // }),
+        // setBicycleSearchQuery(query: string) {
+        //     self.searchBicycleQuery = query;
+        // },
+        // setUserSearchQuery(query: string) {
+        //     self.searchUserQuery = query;
+        // },
+        // setNavtitle(title: string) {
+        //     self.navigationTitle = title;
+        // }
+    });
 
 type IBicycleStore = Instance<typeof BicycleStore>;
 export type { IBicycleStore };
--- rent-a-bicycle/src/store/models/User.ts
+++ rent-a-bicycle/src/store/models/User.ts
@@ -5,26 +5,28 @@ export const User = types.model("User", {
     id: types.number,
     name: types.string,
     funds: types.number
-}).actions(self => ({
-    loadUser: flow(function* (userId: string) {
-        yield userService.fetchUser(userId)
+}).actions(self => {
+    // Helper function to fetch and apply user data
+    function fetchAndUpdateUser(userId: string) {
+        return userService.fetchUser(userId)
             .then((result: any) => {
                 if (result) {
                     applySnapshot(self, result);
                 }
-            })
-    }),
-    addFunds: flow(function* (funds: string) {
-        yield userService.addFundsToUser(self.id.toString(), parseInt(funds, 10))
-        yield userService.fetchUser(self.id.toString())
-            .then((results: any) => {
-                if (results) {
-                   applySnapshot(self,results);
-                }
-            })
+            });
+    }
 
-    })
-}))
+    return {
+        loadUser: flow(function* (userId: string) {
+            yield fetchAndUpdateUser(userId);
+        }),
+
+        addFunds: flow(function* (funds: string) {
+            yield userService.addFundsToUser(self.id.toString(), parseInt(funds, 10));
+            yield fetchAndUpdateUser(self.id.toString());
+        })
+    };
+});
 
 type IUser = Instance<typeof User>;
 export type { IUser }
